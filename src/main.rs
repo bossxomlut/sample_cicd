14394c8f7effd0ce2e43c9fcc884b61fe35aa7b8
Kiều Phong BB'm Real <kieuphong2840@gmail.com>
2026-02-25 12:53:21 +0700

    create project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    "Hello World 1999"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
