4ca95b7eccff4f4f32b9d6cfaf3ab43e9657b10e
Kiều Phong BB'm Real <kieuphong2840@gmail.com>
2026-02-26 10:28:58 +0700

    123

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,15 @@ use std::env;
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
-     // Load `.env` if present (created by CI)
-    let _ = dotenv();
-
     let env_name = env::var("ENVIRONMENT_NAME").unwrap_or_else(|_| "dev".to_string());
-    println!("ENVIRONMENT_NAME={}", env_name);
-
-    format!("Hello World 1999 v2\nBuild build Github Action\nENVIRONMENT_NAME={}", env_name) 
+    format!("Hello World 1999 v2\nBuild build Github Action\nENVIRONMENT_NAME={}", env_name)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Load `.env` once at startup (if present)
+    let _ = dotenv();
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
